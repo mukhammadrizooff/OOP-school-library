5e7a763d04063b26395a8eb9f27101aa3c7c6e34
mukhammadrizooff <legend.it@bk.ru>
2022-04-20 22:30:31 +0500

    Add rental, book classes and fix linters

--- student.rb
+++ student.rb
@@ -12,8 +12,8 @@ class Student < Person
     "¯\(ツ)/¯"
   end
 
-  def classroom = (classroom)
+  def classroom=(classroom)
     @classroom = classroom
-    classroom.student.push(self) unless classroom.students.include?(self)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
